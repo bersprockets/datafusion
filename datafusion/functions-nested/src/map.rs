156892e390ffe30b69008c03bf44ccb9c36cb5dc
Bruce Robbins <bersprockets@gmail.com>
2024-12-27 15:34:43 -0800

    Brute force fix for testing

--- datafusion/functions-nested/src/map.rs
+++ datafusion/functions-nested/src/map.rs
@@ -95,8 +95,15 @@ fn make_map_batch(args: &[ColumnarValue]) -> Result<ColumnarValue> {
         }
     }
 
-    let values = get_first_array_ref(&args[1])?;
-    make_map_batch_internal(keys, values, can_evaluate_to_const, args[0].data_type())
+    return if can_evaluate_to_const {
+        let values = get_first_array_ref(&args[1])?;
+        make_map_scalar_internal(keys, values)
+    } else {
+        let array_args = ColumnarValue::values_to_arrays(args)?;
+        let keys = &array_args[0];
+        let values = &array_args[1];
+        make_map_batch_internal(keys, values, can_evaluate_to_const, args[0].data_type())
+    }
 }
 
 fn check_unique_keys(array: &dyn Array) -> Result<()> {
@@ -124,9 +131,52 @@ fn get_first_array_ref(columnar_value: &ColumnarValue) -> Result<ArrayRef> {
     }
 }
 
-fn make_map_batch_internal(
+fn make_map_scalar_internal(
     keys: ArrayRef,
-    values: ArrayRef,
+    values: ArrayRef
+) -> Result<ColumnarValue> {
+    if keys.len() != values.len() {
+        return exec_err!("map requires key and value lists to have the same length");
+    }
+
+    let key_field = Arc::new(Field::new("key", keys.data_type().clone(), false));
+    let value_field = Arc::new(Field::new("value", values.data_type().clone(), true));
+    let mut entry_struct_buffer: VecDeque<(Arc<Field>, ArrayRef)> = VecDeque::new();
+    let mut entry_offsets_buffer = VecDeque::new();
+    entry_offsets_buffer.push_back(0);
+
+    entry_struct_buffer.push_back((Arc::clone(&key_field), Arc::clone(&keys)));
+    entry_struct_buffer.push_back((Arc::clone(&value_field), Arc::clone(&values)));
+    entry_offsets_buffer.push_back(keys.len() as u32);
+
+    let entry_struct: Vec<(Arc<Field>, ArrayRef)> = entry_struct_buffer.into();
+    let entry_struct = StructArray::from(entry_struct);
+
+    let map_data_type = DataType::Map(
+        Arc::new(Field::new(
+            "entries",
+            entry_struct.data_type().clone(),
+            false,
+        )),
+        false,
+    );
+
+    let entry_offsets: Vec<u32> = entry_offsets_buffer.into();
+    let entry_offsets_buffer = Buffer::from(entry_offsets.to_byte_slice());
+
+    let map_data = ArrayData::builder(map_data_type)
+        .len(entry_offsets.len() - 1)
+        .add_buffer(entry_offsets_buffer)
+        .add_child_data(entry_struct.to_data())
+        .build()?;
+    let map_array = Arc::new(MapArray::from(map_data));
+
+    Ok(ColumnarValue::Scalar(ScalarValue::try_from_array(map_array.as_ref(), 0)?))
+}
+
+fn make_map_batch_internal(
+    keys: &ArrayRef,
+    values: &ArrayRef,
     can_evaluate_to_const: bool,
     data_type: DataType,
 ) -> Result<ColumnarValue> {
@@ -139,9 +189,11 @@ fn make_map_batch_internal(
             make_map_array_internal::<i64>(keys, values)
         } else {
             make_map_array_internal::<i32>(keys, values)
-        };
-    }
+        }
+    };
 
+    // get back the scalar values
+    
     let key_field = Arc::new(Field::new("key", keys.data_type().clone(), false));
     let value_field = Arc::new(Field::new("value", values.data_type().clone(), true));
     let mut entry_struct_buffer: VecDeque<(Arc<Field>, ArrayRef)> = VecDeque::new();
@@ -363,18 +415,22 @@ fn get_element_type(data_type: &DataType) -> Result<&DataType> {
 /// +-----------+      +-----------+
 /// ```text
 fn make_map_array_internal<O: OffsetSizeTrait>(
-    keys: ArrayRef,
-    values: ArrayRef,
+    keys: &ArrayRef,
+    values: &ArrayRef,
 ) -> Result<ColumnarValue> {
     let mut offset_buffer = vec![O::zero()];
     let mut running_offset = O::zero();
 
-    let keys = list_to_arrays::<O>(&keys);
-    let values = list_to_arrays::<O>(&values);
+    let keys = list_to_arrays::<O>(keys);
+    let values = list_to_arrays::<O>(values);
 
     let mut key_array_vec = vec![];
     let mut value_array_vec = vec![];
     for (k, v) in keys.iter().zip(values.iter()) {
+        // check that the two arrays are the same length
+        if k.len() != v.len() {
+            return exec_err!("map requires key and value lists to have the same length");
+        }
         running_offset = running_offset.add(O::usize_as(k.len()));
         offset_buffer.push(running_offset);
         key_array_vec.push(k.as_ref());
